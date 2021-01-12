3fb2d35cbab24f84260948c6557fd6c3c7ee4471
amepro <amedeo.pro@assistdigital.com>
2021-01-12 16:39:42 +0100

    created semple UI

--- src/App.js
+++ src/App.js
@@ -20,10 +20,38 @@ componentDidMount(){
   render() {
     return(
       <div className="App">
-        <h1>Lista Locali Attivi</h1>
-        <ul style={{listStyle: "none"}}>
-        { this.state.locali.map(locale => <li>{locale.nomeLocale}</li>)}
-      </ul>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 mt-5 mb-3">
+              <h1>Gestione Locali APP Ferentino</h1>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row d-flex justify-content-between">
+            <div className="col-lg-6 col-xs-12 box">
+              { this.state.locali.map(locale =>
+                  <div className="row mb-3 mt-3">
+                    <div className="col-lg-3">
+                      <div className="immagineLocale" style={{backgroundImage : `url(${locale.imgurl})`}}></div>
+                    </div>
+                    <div className="col-lg-9 d-flex flex-column justify-content-center text-left">
+                      <p className="font-weight-bold">{locale.nomeLocale}</p>
+                      <p><small>{locale.indirizzo}</small></p>
+                    </div>
+                  </div>)}
+              {/*<ul>*/}
+              {/*  { this.state.locali.map(locale => <li><div className="immagineLocale" style={{backgroundImage : `url(${locale.imgurl})`}}></div><p>{locale.nomeLocale}</p><div>{locale.indirizzo}</div></li>)}*/}
+              {/*</ul>*/}
+            </div>
+            <div className="col-lg-6 col-xs-12">
+                <button className="btn btn-primary mr-2">Inserisci un nuovo Locale</button>
+                <button className="btn btn-primary mr-2">Modifica un Locale</button>
+                <button className="btn btn-primary mr-2">Elimina un Locale</button>
+                <button className="btn btn-primary mt-3">Aggiorna Lista</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
